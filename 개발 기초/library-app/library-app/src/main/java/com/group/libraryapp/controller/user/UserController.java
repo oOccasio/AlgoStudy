7dacef248bc3098cccb2532332f249c9b28eadd3
oOccasio <dhzktldh@gmail.com>
2025-02-07 22:40:59 +0900

    유저 update, delete API 개발

--- 개발 기초/library-app/library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ 개발 기초/library-app/library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -2,13 +2,12 @@ package com.group.libraryapp.controller.user;
 
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -41,6 +40,17 @@ public class UserController {
         });
     }
 
+    @PutMapping("/user")
+    public void updateUser(@RequestBody UserUpdateRequest request){
+        String sql = "UPDATE user SET name = ? WHERE id = ?";
+        jdbcTemplate.update(sql, request.getName(), request.getId());
+    }
+
+    @DeleteMapping("/user")
+    public void deleteUser(@RequestParam String name){
+        String sql = "DELETE FROM user WHERE name = ?";
+        jdbcTemplate.update(sql, name);
+    }
 
 
 }
