36d66392bd8ea6800c0ef6f1388d796fcec0a5b4
oOccasio <dhzktldh@gmail.com>
2025-02-09 05:25:19 +0900

    스프링컨테이너 다루기

---
+++ 개발 기초/library-app/library-app/src/main/java/com/group/libraryapp/repository/book/BookMemoryRepository.java
@@ -0,0 +1,12 @@
+package com.group.libraryapp.repository.book;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookMemoryRepository implements BookRepository {
+
+    @Override
+    public void saveBook(){
+
+    }
+}
