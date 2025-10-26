70ec4c60f7907e1a3307e7ace05b7168b094e02e
oOccasio <dhzktldh@gmail.com>
2025-10-26 23:08:28 +0900

    dp - 멀리뛰기

--- 프로그래머스/2단계/멀리뛰기.cpp
+++ 프로그래머스/2단계/멀리뛰기.cpp
@@ -7,18 +7,19 @@ long long solution(int n) {
     long long answer = 0;
     
     if(n == 1){
-        answer = 1;
+        answer = 1; // 1일 경우 1
     }
     else if(n == 2) {
-        answer = 2;
+        answer = 2; // 2일 경우 2
     } else{
-        int a1 = 1;
+        int a1 = 1; // 초기값세팅
         int a2 = 2;
         int an;
         for(int i = 3; i <= n; i++){
-            an = (a1 + a2) % 1234567;
-            a1 = a2;
-            a2 = an;
+            an = (a1 + a2) % 1234567; // n은 (n - 1) + (n - 2)  
+            a1 = a2;                  // (n - 1)에서 1칸 뛰는 방법이랑
+            a2 = an;                  // (n - 2)에서 2칸 뛰는 방법을 
+                                      // 더하면 n칸째의 방법을 얻을 수있음
         }
         answer = an;
     }
