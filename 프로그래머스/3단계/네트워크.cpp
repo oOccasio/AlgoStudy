9df74a5e7dad69db06ad88f012369440a4d9a646
oOccasio <dhzktldh@gmail.com>
2025-10-29 18:39:05 +0900

    DFS - 네트워크

--- 프로그래머스/3단계/네트워크.cpp
+++ 프로그래머스/3단계/네트워크.cpp
@@ -3,11 +3,11 @@
 
 using namespace std;
 void dfs (int n, vector<vector<int>> &computers, vector<int> &visit, int k ){
-    if(visit[k] == 0){
-        visit[k] = 1;
+    if(visit[k] == 0){ // 방문하지 않았으면
+        visit[k] = 1;   // 방문처리 
         for(int i = 0 ; i < n; i++){
-            if(computers[k][i] == 1 && k != i){
-                dfs(n, computers, visit, i);
+            if(computers[k][i] == 1 && k != i){  // 연결되어있고 자기자신이 아니면
+                dfs(n, computers, visit, i); // 재귀호출
             }
         }  
     }
